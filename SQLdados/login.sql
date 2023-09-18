36a175a2a3d74f6be751d4b238a5dd411b5b6c28
Wladison Maciel <125041870+Wladison-Maciel@users.noreply.github.com>
2023-09-17 22:16:05 -0300

    Add files via upload

---
+++ SQLdados/login.sql
@@ -0,0 +1,11 @@
+create table logar (
+nome varchar(40),
+email varchar(40),
+senha varchar(12)
+)
+
+select * from logar
+
+insert into logar (nome, email, senha) values ();
+
+select nome,senha from logar where nome = 'nicolas'and senha = '09112006';
